cef9db0a21bcc35007215c6ade3938ad9fd659c1
Romstar744 <127832768+Romstar744@users.noreply.github.com>
2023-12-03 23:56:10 +0300

    Add files via upload

---
+++ ПР11_Модули/ПР11_Задание2/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections;
+
+namespace ПР11_2
+{
+    public struct Work
+    {
+        public string kod;
+        public string fullName;
+        public string position;
+        public uint salary;
+        public int experience;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList employees = new ArrayList();
+
+            // Заполнение начальных данных
+            employees.Add(new Work { kod = "001", fullName = "Иванов Иван", position = "повар", salary = 25000, experience = 3 });
+            employees.Add(new Work { kod = "002", fullName = "Петров Петр", position = "кондитер", salary = 15842, experience = 2 });
+            employees.Add(new Work { kod = "003", fullName = "Сидорова Ирина", position = "пекарь", salary = 47852, experience = 4 });
+            employees.Add(new Work { kod = "004", fullName = "Кузнецова Ольга", position = "тестомес", salary = 25871, experience = 1 });
+            employees.Add(new Work { kod = "005", fullName = "Федоров Ян", position = "кондитер", salary = 35890, experience = 5 });
+
+            DisplayEmployees(employees);
+
+            while (true)
+            {
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1. Добавить запись");
+                Console.WriteLine("2. Удалить запись");
+                Console.WriteLine("3. Найти количество сотрудников по должности");
+                Console.WriteLine("4. Вывести данные в обратном порядке");
+                Console.WriteLine("5. Найти и вывести сотрудников со стажем менее 5 лет и окладом выше среднего");
+                Console.WriteLine("6. Выйти из программы");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddEmployee(employees);
+                        DisplayEmployees(employees);
+                        break;
+
+                    case "2":
+                        RemoveEmployee(employees);
+                        DisplayEmployees(employees);
+                        break;
+
+                    case "3":
+                        CountEmployeesByPosition(employees);
+                        break;
+
+                    case "4":
+                        DisplayEmployeesReversed(employees);
+                        break;
+
+                    case "5":
+                        DisplayHighSalaryLowExperienceEmployees(employees);
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Выход из программы.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Некорректный выбор. Пожалуйста, повторите.");
+                        break;
+                }
+            }
+        }
+
+        static void DisplayEmployees(ArrayList employees)
+        {
+            Console.WriteLine("\nТекущее содержимое списка сотрудников:\n");
+
+            foreach (Work employee in employees)
+            {
+                Console.WriteLine($"{employee.kod} {employee.fullName} {employee.position} {employee.salary} {employee.experience}");
+            }
+        }
+
+        static void AddEmployee(ArrayList employees)
+        {
+            Console.Write("\nВведите код сотрудника: ");
+            string kod = Console.ReadLine();
+
+            Console.Write("Введите Ф.И.: ");
+            string fullName = Console.ReadLine();
+
+            Console.Write("Введите должность: ");
+            string position = Console.ReadLine();
+
+            Console.Write("Введите оклад: ");
+            uint salary = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("Введите стаж: ");
+            int experience = Convert.ToInt32(Console.ReadLine());
+
+            employees.Add(new Work { kod = kod, fullName = fullName, position = position, salary = salary, experience = experience });
+
+            Console.WriteLine("Запись добавлена успешно.");
+        }
+
+        static void RemoveEmployee(ArrayList employees)
+        {
+            Console.Write("\nВведите код уволенного сотрудника: ");
+            string kod = Console.ReadLine();
+
+            int index = FindEmployeeIndexByCode(employees, kod);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"\nУволенный сотрудник: {((Work)employees[index]).fullName}\n");
+
+                employees.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Сотрудник с таким кодом не найден.");
+            }
+        }
+
+        static void CountEmployeesByPosition(ArrayList employees)
+        {
+            Console.Write("\nВведите должность для подсчета: ");
+            string position = Console.ReadLine();
+
+            int count = 0;
+
+            foreach (Work employee in employees)
+            {
+                if (employee.position.Equals(position, StringComparison.OrdinalIgnoreCase))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Количество сотрудников на должности {position}: {count}");
+        }
+
+        static void DisplayEmployeesReversed(ArrayList employees)
+        {
+            Console.WriteLine("\nСписок сотрудников в обратном порядке:");
+
+            for (int i = employees.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"{((Work)employees[i]).kod} {((Work)employees[i]).fullName} " +
+                                  $"{((Work)employees[i]).position} {((Work)employees[i]).salary} {((Work)employees[i]).experience}");
+            }
+        }
+
+        static void DisplayHighSalaryLowExperienceEmployees(ArrayList employees)
+        {
+            double averageSalary = CalculateAverageSalary(employees);
+
+            Console.WriteLine($"\nСотрудники со стажем менее 5 лет и окладом выше среднего ({averageSalary}):");
+
+            foreach (Work employee in employees)
+            {
+                if (employee.experience < 5 && employee.salary > averageSalary)
+                {
+                    Console.WriteLine($"{employee.fullName} {employee.salary} {employee.experience} лет");
+                }
+            }
+        }
+
+        static double CalculateAverageSalary(ArrayList employees)
+        {
+            double totalSalary = 0;
+
+            foreach (Work employee in employees)
+            {
+                totalSalary += employee.salary;
+            }
+
+            return totalSalary / employees.Count;
+        }
+
+        static int FindEmployeeIndexByCode(ArrayList employees, string code)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                if (((Work)employees[i]).kod.Equals(code, StringComparison.OrdinalIgnoreCase))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ ПР11_Модули/ПР_11_Задание1/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace ПР11
+{
+	
+	public struct Work
+	{
+		public int kod; // поле кода сотрудника
+		public double zarabotok; // поле заработной платы
+		public string name; // поле имя сотрудника
+		public string surname; // поле фамилия сотрудника
+		public string cardNumber; // поле номер карты сотрудника
+		public string phoneNumber; // поле номер телефона сотрудника
+	}
+
+	class Program
+	{
+		enum Name { Иван, Петр, Ирина, Ольга, Ян }
+		enum Surname { Сидоров, Андреев, Иванова, Выходова, Юрин }
+
+		private static Type t1 = typeof(Name);
+		private static Type t2 = typeof(Surname);
+
+		static void Main(string[] args)
+		{
+			int k = 0;
+			Work[] w = new Work[5]; // объявление структурной переменной и выделение памяти для массива
+			Name[] names;
+			// Получить значения констант из перечисления Name
+			names = Enum.GetValues(typeof(Name)).Cast<Name>().ToArray();
+			Surname[] surnames;
+			// Получить значения констант из перечисления Surname
+			surnames = Enum.GetValues(typeof(Surname)).Cast<Surname>().ToArray();
+
+			Random r = new Random();
+			for (int i = 0; i< w.Length; i++)
+			{
+				w[i].kod = i;
+
+				if (i < names.Length && i < surnames.Length)
+				{
+					// вводим данные сотрудников: Имя и Фамилия
+					Console.WriteLine("Код сотрудника №" + (i + 1) + ": Имя: " + names[i]);
+					w[i].name = names[i].ToString();
+					Console.WriteLine("Код сотрудника №" + (i + 1) + ": Фамилия: " + surnames[i].ToString() + "\n");
+					w[i].surname = surnames[i].ToString();
+				}
+
+
+
+				// вводим данные сотрудников: Имя и Фамилия
+				//Console.Write(" Код сотрудника №" + (i+1)+": Имя: "+ names[i].ToString());
+				//w[i].name = Console.ReadLine();
+                //Console.Write(" Код сотрудника №" + (i + 1) + ": Фамилия: " + surnames[i].ToString()+ "\n");
+				//w[i].surname = Console.ReadLine();
+
+
+
+				// формируем заработную плату (числа случайным образом в диапазоне от 20000 до 30000)
+				w[i].zarabotok = r.Next(200000, 300000) / 10.0;
+
+				// вводим данные о номере карты и номере телефона
+				Console.WriteLine("Код сотрудника №" + (i + 1) + ": Номер карты: ");
+				w[i].cardNumber = Console.ReadLine();
+
+				Console.WriteLine("Код сотрудника №" + (i + 1) + ": Номер телефона: ");
+				w[i].phoneNumber = Console.ReadLine();
+			}
+            // Вывод массива строк в цикле
+            Console.WriteLine("\n Результат:");
+			for (int i = 0; i< w.Length;i++)
+			{
+				Console.WriteLine("Код сотрудника №" + (i + 1) + ": " + w[i].name + " " + w[i].surname +
+								  " Зарплата - " + w[i].zarabotok + " Номер карты - " + w[i].cardNumber +
+								  " Номер телефона - " + w[i].phoneNumber);
+			}
+
+			Console.WriteLine("\n Проверка корректности ввода:");
+			foreach (var employee in w)
+			{
+				ValidateCardNumber(employee.cardNumber);
+				ValidatePhoneNumber(employee.phoneNumber);
+			}
+
+			// Определение количества сотрудников с зарплатой больше прожиточного минимума
+			Console.WriteLine("\n Количество сотрудников с зарплатой больше 20000 рублей: " +
+							 w.Count(emp => emp.zarabotok > 20000));
+
+			// Вывод фамилий сотрудников, начинающихся на А
+			Console.WriteLine("\n Фамилии сотрудников, начинающихся на А:");
+			foreach (var employee in w.Where(emp => emp.surname != null && emp.surname.StartsWith("А")))
+			{
+				Console.WriteLine(employee.surname);
+			}
+
+			// Вывод списка фамилий, отсортированных по алфавиту
+			Console.WriteLine("\n Список фамилий, отсортированных по алфавиту:");
+			foreach (var employee in w.OrderBy(emp => emp.surname))
+			{
+				Console.WriteLine(employee.surname);
+			}
+
+			Console.ReadKey();
+        }
+
+		static void ValidateCardNumber(string cardNumber)
+		{
+			// Регулярное выражение для номера карты (пример)
+			string pattern = @"^\d{4}-\d{4}-\d{4}-\d{4}$";
+
+			if (Regex.IsMatch(cardNumber, pattern))
+			{
+				Console.WriteLine($"Номер карты {cardNumber} введен корректно.");
+			}
+			else
+			{
+				Console.WriteLine($"Некорректный номер карты: {cardNumber}");
+			}
+		}
+
+		static void ValidatePhoneNumber(string phoneNumber)
+		{
+			// Регулярное выражение для номера телефона (пример)
+			string pattern = @"^\d{3}-\d{2}-\d{2}$";
+
+			if (Regex.IsMatch(phoneNumber, pattern))
+			{
+				Console.WriteLine($"Номер телефона {phoneNumber} введен корректно.");
+			}
+			else
+			{
+				Console.WriteLine($"Некорректный номер телефона: {phoneNumber}");
+			}
+		}
+	}
+}
